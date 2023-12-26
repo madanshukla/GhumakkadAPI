0c9b1b29bff9495874bd380c194f085200a55902
madanshukla <shukla.madan267@gmail.com>
2023-12-26 18:39:56 +0530

    command to get from db

--- Database/DeploymentScript/Script.sql
+++ Database/DeploymentScript/Script.sql
@@ -1,3 +1,5 @@
+dotnet ef dbcontext scaffold "Server=DESKTOP-2VPR9IB\LOCALHOST;Database=Ghumakkad;;User ID=sa;Password=sql;TrustServerCertificate=True" Microsoft.EntityFrameworkCore.SqlServer -o Database
+
 create table Articles(ArticleId int identity(1,1) primary key, Title varchar(max), Description text, State int,IsActive int,
 CreatedBy int, CreatedDate Date,
 UpdatedBy int, UpdatedDate Date
